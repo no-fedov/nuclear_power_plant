dd0ba31292f1d4bfd57e725c2358eb3b1b6cb54f
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-11-25 17:19:00 +0300

    fix: убрал аннотацию @RequiredArgsConstructor в EconomicProperty

--- src/main/java/com/javaacademy/config/EconomicProperty.java
+++ src/main/java/com/javaacademy/config/EconomicProperty.java
@@ -1,7 +1,6 @@
 package com.javaacademy.config;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -9,7 +8,6 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 @ConfigurationProperties("app.economic")
 public class EconomicProperty {
     private String currency;
